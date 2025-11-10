8da13606eeb62e743c40979377bebd29c4dfdfa9
AndrewMicheal97 <a.micheal@masaref-bsc.com>
2025-11-10 08:46:09 +0200

    chore(development): added core, common, and features structure
    - In core:
      Added files: app_colors, app_routes, assets_manager, and app_styles
    
    - In common:
      Added widgets folder containing:
        - custom_elevated_button
        - custom_text_form_field
    
    - In features:
      (empty for now / to be implemented later)

---
+++ lib/core/assets_manager.dart
@@ -0,0 +1,29 @@
+class AssetsManager {
+
+  static const String onBoardingImage1 = "assets/images/onBoardingImage1.png";
+  static const String onBoardingImage2 = "assets/images/discoverMoviesImage.png";
+  static const String onBoardingImage3 = "assets/images/exploreAllGenres.png";
+  static const String onBoardingImage4 = "assets/images/watchListImage.png";
+  static const String onBoardingImage5 = "assets/images/rateImage.png";
+  static const String onBoardingImage6 = "assets/images/startWatchImage.png";
+
+  static const String avatarImage1 = "assets/images/1.png";
+  static const String avatarImage2 = "asset/images/2.png";
+  static const String avatarImage3 = "assets/images/3.png";
+
+  static const String emptyListImage = "assets/images/Empty.png";
+  static const String forgotPasswordImage = "assets/images/forgotPasswordImage.png";
+
+  static const String loginSplashScreen = "assets/images/login_splash_image.png";
+  static const String iconMessage = "assets/images/iconMessage.png";
+  static const String iconPassword = "assets/images/iconPassword.png";
+  static const String iconGoogle = "assets/images/icon_google.png";
+
+  static const String iconIdentification = "assets/images/iconIdentification_.png";
+  static const String iconPhone = "assets/images/iconPhone.png";
+  static const String iconBookmark = "assets/images/iconBookmark.png";
+
+  static const String americaIcon = "assets/images/america_icon.png";
+  static const String egyptIcon = "assets/images/EG.png";
+  
+}
