8270e36598edbb3e54f200b986bb3e321c4cfb41
AndrewMicheal97 <a.micheal@masaref-bsc.com>
2025-11-14 19:37:45 +0200

    feat(login): added SharedPreferences in login

--- lib/features/auth/presentation/cubit/login_cubit.dart
+++ lib/features/auth/presentation/cubit/login_cubit.dart
@@ -2,19 +2,31 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:movies/features/auth/domain/usecases/login_use_case.dart';
 import 'package:movies/features/auth/presentation/cubit/login_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @injectable
 class LoginCubit extends Cubit<LoginState>{
   LoginUseCase loginuseCase;
+
   LoginCubit(this.loginuseCase): super(LoginInitial());
 
-  Future<void> login(String email , String password)async{
-    try {
-      emit(LoginLoadingState());
-       var result = await loginuseCase.login(email, password);
-       result.fold(ifLeft: (errMsg) => emit(LoginErrorState(errorMessage: errMsg.message)), ifRight: (success) => emit(LoginSuccessState(success , success.message)));
-    } catch (e) {
-      emit(LoginErrorState(errorMessage: e.toString()));
-    }
+  Future<void> login(String email, String password) async {
+  emit(LoginLoadingState());
+  try {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    var result = await loginuseCase.login(email, password);
+    
+    result.fold(
+      ifLeft: (errMsg) => emit(LoginErrorState(errorMessage: errMsg.message)),
+      ifRight: (success) async {
+        print("TOKEN: ${success.data}");
+        await prefs.setString('token', success.data ?? "");
+        emit(LoginSuccessState(success, success.message));
+      },
+    );
+  } catch (e) {
+    emit(LoginErrorState(errorMessage: e.toString()));
   }
+}
 }
